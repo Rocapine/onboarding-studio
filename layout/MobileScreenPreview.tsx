5bb5e8340d9f89d8eb82df998a935586a82a064f
Sammy Teillet <sammy.teillet@gmail.com>
2024-10-23 20:43:38 +0200

    feature: use headings instead of text

--- layout/MobileScreenPreview.tsx
+++ layout/MobileScreenPreview.tsx
@@ -1,5 +1,7 @@
 import { View, Text } from "@tamagui/core";
+import { H3, H6 } from "tamagui";
 import { useSteps } from "../contexts/steps-context";
+import { Paragraph } from "tamagui";
 
 export default function MobileScreenPreview() {
   const { selectedStep } = useSteps();
@@ -7,11 +9,11 @@ export default function MobileScreenPreview() {
     <View flex={1} padding={20} alignItems="center" justifyContent="center" backgroundColor={"$accentBackground"}>
       <View width={375} height={812} borderRadius={40} overflow="hidden" borderColor={"$borderColor"} backgroundColor={"$background"} borderWidth={2}>
         <View height={44} backgroundColor={"$statusBarBackground"} justifyContent="center" alignItems="center">
-          <Text color={"$statusBarTextColor"}>9:41 AM</Text>
+          <H6 color={"$white"}>9:41 AM</H6>
         </View>
         <View flex={1} justifyContent="center" alignItems="center" gap="$4" backgroundColor={"$screenBackground"}>
-          <Text >{selectedStep?.type}</Text>
-          <Text >{selectedStep?.name}</Text>
+          <H3>{selectedStep?.type}</H3 >
+          <Paragraph color={"$pink"}>{selectedStep?.name}</Paragraph>
         </View>
       </View>
     </View>
