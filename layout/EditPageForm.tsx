f6001e80617a165f13f6117955301deff1b291d0
Sammy Teillet <sammy.teillet@gmail.com>
2024-10-24 18:16:23 +0200

    feature: edit name of step

--- layout/EditPageForm.tsx
+++ layout/EditPageForm.tsx
@@ -1,10 +1,11 @@
 import { View } from "@tamagui/core";
 import { useSteps } from "../contexts/steps-context";
-import { Heading, YStack } from "tamagui";
+import { Heading, Input, TextArea, YStack } from "tamagui";
 import React from "react";
 import { SelectType } from "../components/SelectType";
 import { getInitialStepPayload, StepProperties, StepType } from "../contexts/step.type";
 import { MediaContentEditor } from "../OnboardingSteps/MediaContent/MediaContent.form";
+import { NativeSyntheticEvent, TextInputChangeEventData } from "react-native";
 
 
 export default function EditPageForm() {
@@ -15,6 +16,13 @@ export default function EditPageForm() {
     if (selectedStep) {
       const initialPayload = getInitialStepPayload(type)
       setStep(selectedStep.id, { ...selectedStep, type, payload: initialPayload, } as StepProperties);
+    }
+  }, [selectedStep, setStep]);
+
+  const handleNameChange = React.useCallback((event: NativeSyntheticEvent<TextInputChangeEventData>) => {
+    if (selectedStep) {
+      const textWithoutNewLine = event.nativeEvent.text.replace(/\n/g, '');
+      setStep(selectedStep.id, { ...selectedStep, name: textWithoutNewLine });
     }
   }, [selectedStep, setStep]);
 
@@ -33,7 +41,13 @@ export default function EditPageForm() {
     <View flex={1} padding={"$4"} alignItems="center" justifyContent="flex-start">
       {selectedStep ? (
         <YStack gap="$4" width="$20">
-          <Heading>{selectedStep.name}</Heading>
+          <TextArea
+            value={selectedStep.name}
+            onChange={handleNameChange}
+            placeholder="Enter step name"
+            lineHeight={1.2 * 24}
+            style={{ fontSize: 24, fontWeight: 'bold', borderColor: 'transparent', borderWidth: 0, padding: 0, backgroundColor: 'transparent' }} // Apply heading styles
+          />
           <SelectType selectedType={selectedStep.type} setSelectedType={setSelectedType} />
           {editor}
         </YStack>
