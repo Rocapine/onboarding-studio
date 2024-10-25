f99e4f5f44b9029970ff0c883d4357db04601659
Sammy Teillet <sammy.teillet@gmail.com>
2024-10-25 19:23:46 +0200

    feature: display export of onboarding

--- layout/ExportSheet.tsx
+++ layout/ExportSheet.tsx
@@ -1,10 +1,10 @@
-import { Sheet, Button, Input } from "tamagui"
+import { Sheet, Button, Input, View, Paragraph } from "tamagui"
 import React from "react"
 import { ChevronDown } from "@tamagui/lucide-icons"
 
 export const ExportSheet = ({ open, setOpen }: { open: boolean, setOpen: (open: boolean) => void }) => {
   const [position, setPosition] = React.useState(0)
-
+  const jsonString = "{\"users\":[{\"id\":1,\"name\":\"John Doe\",\"email\":\"john.doe@example.com\",\"address\":{\"street\":\"123 Main St\",\"city\":\"Anytown\",\"state\":\"CA\",\"zip\":\"12345\"},\"posts\":[{\"id\":101,\"title\":\"My First Post\",\"content\":\"This is the content of my first post.\"},{\"id\":102,\"title\":\"Another Post\",\"content\":\"This is some more content.\"}]},{\"id\":2,\"name\":\"Jane Smith\",\"email\":\"jane.smith@example.com\",\"address\":{\"street\":\"456 Elm St\",\"city\":\"Othertown\",\"state\":\"NY\",\"zip\":\"67890\"},\"posts\":[{\"id\":103,\"title\":\"Hello World\",\"content\":\"Hello, this is Jane!\"}]},{\"id\":3,\"name\":\"Alice Johnson\",\"email\":\"alice.johnson@example.com\",\"address\":{\"street\":\"789 Oak St\",\"city\":\"Sometown\",\"state\":\"TX\",\"zip\":\"11223\"},\"posts\":[{\"id\":104,\""
   return (
     <Sheet
       forceRemoveScrollEnabled={open}
@@ -26,9 +26,10 @@ export const ExportSheet = ({ open, setOpen }: { open: boolean, setOpen: (open: 
 
       <Sheet.Handle />
       <Sheet.Frame padding="$4" justifyContent="center" alignItems="center" gap="$5" backgroundColor={"black"}>
-        <Button size="$6" circular icon={ChevronDown} onPress={() => setOpen(false)} />
-        <Input width={200} />
-
+        <View height={"100%"} width={"100%"} backgroundColor={"white"} >
+          <Paragraph height={"100%"} >UN FAUX JSON</Paragraph>
+          <Paragraph height={"100%"} >{jsonString}</Paragraph>
+        </View>
       </Sheet.Frame>
     </Sheet>
   )
