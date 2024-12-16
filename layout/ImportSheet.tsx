d1df98ed2314515b40166b7c2a4c64e296eb1f39
Sammy Teillet <sammy.teillet@gmail.com>
2024-12-16 22:54:57 +0100

    feature: assert json is valid step with unique ids

--- layout/ImportSheet.tsx
+++ layout/ImportSheet.tsx
@@ -1,10 +1,20 @@
 import { useState } from 'react';
 import { Button, TextArea, Sheet, Text, XStack, YStack } from 'tamagui';
 import { useSteps } from '../contexts/steps-context';
+import { OnboardingStep } from '../contexts/step.type';
+
+const assertHasUniqueIds = (steps: OnboardingStep[]) => {
+  const ids = steps.map(step => step.id);
+  const uniqueIds = new Set(ids);
+  if (uniqueIds.size !== steps.length) {
+    throw new Error("Steps must have unique IDs");
+  }
+}
+
 
 export const ImportSheet = ({ open, setOpen, setSteps }: { open: boolean, setOpen: (open: boolean) => void, setSteps: (steps: OnboardingStep[]) => void }) => {
   const [jsonText, setJsonText] = useState('');
-  const [isValidJson, setIsValidJson] = useState(true);
+  const [parseJsonError, setParseJsonError] = useState<string | null>(null);
 
   const handleSave = () => {
     try {
@@ -12,9 +22,9 @@ export const ImportSheet = ({ open, setOpen, setSteps }: { open: boolean, setOpe
       localStorage.setItem('onboardingSteps', JSON.stringify(parsedJson));
       setSteps(parsedJson);
       setOpen(false);
-    } catch (error) {
+    } catch (error: any) {
       console.error('Invalid JSON:', error);
-      setIsValidJson(false);
+      setParseJsonError(error.message || "Unknown error");
     }
   };
 
@@ -22,9 +32,10 @@ export const ImportSheet = ({ open, setOpen, setSteps }: { open: boolean, setOpe
     setJsonText(text);
     try {
       JSON.parse(text);
-      setIsValidJson(true);
-    } catch (error) {
-      setIsValidJson(false);
+      assertHasUniqueIds(JSON.parse(text));
+      setParseJsonError(null);
+    } catch (error: any) {
+      setParseJsonError(error.message || "Unknown error");
     }
   };
 
@@ -41,8 +52,8 @@ export const ImportSheet = ({ open, setOpen, setSteps }: { open: boolean, setOpe
             />
             <XStack justifyContent="center" alignItems="center" gap="$2">
 
-              {!isValidJson ? <Text color="red">JSON: Invalid</Text> : <Text color="green">JSON: Valid</Text>}
-              <Button onPress={handleSave} disabled={!isValidJson}>
+              {parseJsonError ? <Text color="red">JSON Invalid: {parseJsonError}</Text> : <Text color="green">JSON Valid</Text>}
+              <Button onPress={handleSave} disabled={!!parseJsonError}>
                 Import
               </Button>
             </XStack>
