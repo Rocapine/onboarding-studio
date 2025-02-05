e5418e89927e24a51120be3f98e404405fef3768
Sammy Teillet <sammy.teillet@gmail.com>
2025-02-05 07:14:29 +0100

    refactor: clean custom screen page

--- OnboardingSteps/CustomScreen/CustomScreen.page.tsx
+++ OnboardingSteps/CustomScreen/CustomScreen.page.tsx
@@ -1,7 +1,4 @@
-import { YStack, Text, Image, XStack, Stack, View, H3, Paragraph } from "tamagui";
-import { useIPhoneContext } from "../../contexts/iphone-context";
-import { Button } from "../../components/Lib/Button";
-import LottieView from "lottie-react-native";
+import { YStack, H3, Paragraph } from "tamagui";
 import { CustomScreenStepType } from "../step.type";
 
 type ContentProps = {
@@ -11,6 +8,6 @@ type ContentProps = {
 export const CustomScreenStep = ({ step }: ContentProps) => {
   return (<YStack padding="$4" flex={1} justifyContent="center">
     <H3>{step.type}</H3>
-    <Paragraph >{step.name}</Paragraph>
+    <Paragraph>{step.name}</Paragraph>
   </YStack>)
 };
