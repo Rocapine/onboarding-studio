4457124a7c02b6359e277f5d862d8d781bae8c23
Sammy Teillet <sammy.teillet@gmail.com>
2025-03-24 19:53:31 +0100

    feature: add navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,54 @@
 import { Provider } from '@/Provider';
-import { Slot } from 'expo-router';
-import { PortalProvider } from 'tamagui';
+import { Link, Slot } from 'expo-router';
+import { PortalProvider, Stack, H2, Paragraph, YStack } from 'tamagui';
+import { useEffect, useState } from 'react';
+import { supabase } from '@/supabase.client';
 
 export default function RootLayout() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (session) { setIsLoggedIn(true) }
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      if (session) { setIsLoggedIn(true) }
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
   return (
     <Provider>
       <PortalProvider shouldAddRootHost>
-        <Slot />
+        <Stack style={{ flex: 1, flexDirection: 'row' }}>
+          {/* Sidebar */}
+          {isLoggedIn && <Stack
+            backgroundColor={'$background'}
+            padding={'$4'}
+            borderRightWidth={1}
+            borderRightColor={'$borderColor'}
+
+          >
+            <YStack gap={"$2"} >
+              <Link href="/projects">
+                <Paragraph >Projects</Paragraph>
+              </Link>
+              <Link href="/teams">
+                <Paragraph >Teams</Paragraph>
+              </Link>
+
+            </YStack>
+          </Stack>
+          }
+          {/* Main content */}
+          <Stack style={{ flex: 1 }}>
+            <Slot />
+          </Stack>
+        </Stack>
       </PortalProvider>
-    </Provider>
+    </Provider >
   );
 }
