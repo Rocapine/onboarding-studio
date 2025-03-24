798621784048393e42f947f163c88fb6cc46a808
Sammy Teillet <sammy.teillet@gmail.com>
2025-03-24 10:06:35 +0100

    feature: home redirect on auth/login

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,7 @@
-import { View } from '@tamagui/core';
-import { ToastViewport } from "@tamagui/toast";
-import EditPageForm from '../layout/EditPageForm';
-import MobileScreenPreview from "../layout/MobileScreenPreview";
-import StepsList from '../layout/StepsList';
-import { OfflineStepsProvider } from '@/contexts/steps-context';
-import { CurrentToast } from '@/components/CurrentToast';
-
-const Layout = () => {
-  return (
-    <OfflineStepsProvider>
-      <View backgroundColor={"$background"} flex={1} flexDirection="row">
-        <ToastViewport />
-        <CurrentToast />
-        <StepsList />
-        <MobileScreenPreview />
-        <EditPageForm />
-      </View>
-    </OfflineStepsProvider>
-  );
-};
+import { Redirect } from 'expo-router';
 
 export default function App() {
   return (
-    <Layout />
+    <Redirect href={"auth/login"} />
   );
 }
-
