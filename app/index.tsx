5319d3bc6b12f5d91e9d7b5b517534d13af796d8
Sammy Teillet <sammy.teillet@gmail.com>
2025-03-24 09:50:47 +0100

    refactor: move CurrentToast in component

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import { View } from '@tamagui/core';
-import { Toast, ToastViewport, useToastState } from "@tamagui/toast";
-import { YStack } from "tamagui";
+import { ToastViewport } from "@tamagui/toast";
 import EditPageForm from '../layout/EditPageForm';
 import MobileScreenPreview from "../layout/MobileScreenPreview";
 import StepsList from '../layout/StepsList';
 import { OfflineStepsProvider } from '@/contexts/steps-context';
+import { CurrentToast } from '@/components/CurrentToast';
 
 const Layout = () => {
   return (
@@ -19,31 +19,6 @@ const Layout = () => {
     </OfflineStepsProvider>
   );
 };
-const CurrentToast = () => {
-  const currentToast = useToastState()
-
-  if (!currentToast || currentToast.isHandledNatively) return null
-  return (
-    <Toast
-      key={currentToast.id}
-      duration={currentToast.duration}
-      enterStyle={{ opacity: 0, scale: 0.5, y: -25 }}
-      exitStyle={{ opacity: 0, scale: 1, y: -20 }}
-      y={0}
-      opacity={1}
-      scale={1}
-      animation="100ms"
-      viewportName={currentToast.viewportName}
-    >
-      <YStack>
-        <Toast.Title>{currentToast.title}</Toast.Title>
-        {!!currentToast.message && (
-          <Toast.Description>{currentToast.message}</Toast.Description>
-        )}
-      </YStack>
-    </Toast>
-  )
-}
 
 export default function App() {
   return (
