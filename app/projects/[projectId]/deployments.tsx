c34ad298a1ba29ea4e569d0b1470e9020d4c9b54
Sammy Teillet <sammy.teillet@gmail.com>
2025-03-25 15:17:19 +0100

    feature: get projects depoyments

--- app/projects/[projectId]/deployments.tsx
+++ app/projects/[projectId]/deployments.tsx
@@ -1,14 +1,15 @@
 import { useLocalSearchParams } from "expo-router";
-import { H1, Stack } from "tamagui";
+import { H1, H2, Stack } from "tamagui";
 import { useDeployments } from "@/hooks/useDeployments";
 export default function Deployments() {
   const { projectId } = useLocalSearchParams<{ projectId: string }>();
   const deployment = useDeployments(projectId);
+  console.log(deployment.data)
 
   return <Stack backgroundColor={"$background"} flex={1} flexDirection="row">
     <Stack flex={1} backgroundColor={"$background"} padding={"$4"} gap={"$4"}>
-      <H1>Project {deployment.data.projects.name}</H1>
-      <H1>Deployments</H1>
+      <H1>Project {deployment.data.name}</H1>
+      <H2>Deployments</H2>
     </Stack>
   </Stack>
 }
--- hooks/useDeployments.ts
+++ hooks/useDeployments.ts
@@ -4,14 +4,16 @@ import { supabase } from "../supabase.client";
 
 const queryKey = "deployments";
 export const useDeployments = (projectId: Project["id"]) => {
+  console.log("projectId", projectId);
   const query = useSuspenseQuery({
     queryKey: [queryKey],
     queryFn: async () => {
       const { data, error } = await supabase
-        .from("deployments")
-        .select("*, projects(*)")
-        .eq("project_id", projectId)
+        .from("projects")
+        .select("*, deployments(*)")
+        .eq("id", projectId)
         .single();
+
       if (error) {
         throw new Error("Network response was not ok", error);
       }
