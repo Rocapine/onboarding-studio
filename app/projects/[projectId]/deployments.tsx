096977e91c3ba445eae021f0e5bb62450fb8234a
Sammy Teillet <sammy.teillet@gmail.com>
2025-03-26 10:32:52 +0100

    feature: fix url of deployment

--- app/projects/[projectId]/deployments.tsx
+++ app/projects/[projectId]/deployments.tsx
@@ -1,21 +1,21 @@
-import { useLocalSearchParams } from "expo-router";
+import { Link, useLocalSearchParams } from "expo-router";
 import { Button, H1, H2, H3, Paragraph, Stack, XStack } from "tamagui";
 import { useDeployments } from "@/hooks/useDeployments";
 export default function Deployments() {
   const { projectId } = useLocalSearchParams<{ projectId: string }>();
   const deployment = useDeployments(projectId);
-  const url = `${process.env.EXPO_PUBLIC_SUPABASE_URL}/functions/V1/get-onboarding-steps?projectId=${projectId}&environment=production`
+  const url = `${process.env.EXPO_PUBLIC_SUPABASE_URL}/functions/v1/get-onboarding-steps?projectId=${projectId}&environment=production`
 
   return <Stack backgroundColor={"$background"} flex={1} flexDirection="row">
     <Stack flex={1} backgroundColor={"$background"} padding={"$4"} gap={"$4"}>
       <H1>Project {deployment.data.name}</H1>
-      <H2>Deployments</H2>
-      <XStack>
-        <Paragraph>{url}</Paragraph>
+      <XStack gap={'$2'} alignItems="baseline">
+        <H2>Deployments</H2>
+        <Link href={url} target="_blank">
+          <Paragraph>{url}</Paragraph>
+        </Link>
         <Button
           onPress={() => {
-            const url = `${process.env.EXPO_PUBLIC_SUPABASE_URL}/functions/V1/get-onboarding-steps?projectId=${projectId}&environment=production`;
-            // Using the clipboard API
             navigator.clipboard.writeText(url);
           }}
         >
